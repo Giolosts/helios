ed77f78397615846a9ad9379c172883a133cdb70
gerardvcruz <gerardcruz@live.com>
2014-06-08 18:44:22 +0800

    added chart legends

--- application/views/footer.php
+++ application/views/footer.php
@@ -18,6 +18,65 @@
 	<script>
 			var Months = ["January","February","March","April","May","June","July","August","September","October","Novemeber","December"]
 			var ROILabel = <?php print_r($yearsLabel); ?>
+
+			var lineChartOptions1 = {
+				//Boolean - If we show the scale above the chart data			
+				scaleOverlay : true,
+				//Boolean - If we want to override with a hard coded scale
+				scaleOverride : true,
+				//** Required if scaleOverride is true **
+				//Number - The number of steps in a hard coded scale
+				scaleSteps : 10,
+				//Number - The value jump in the hard coded scale
+				scaleStepWidth : Math.ceil(<?php print_r($Amt * 1.05); ?> / 10),
+				//Number - The scale starting value
+				scaleStartValue : 0,
+				//String - Colour of the scale line	
+				scaleLineColor : "rgba(0,0,0,.1)",
+				//Number - Pixel width of the scale line	
+				scaleLineWidth : 1,
+				//Boolean - Whether to show labels on the scale	
+				scaleShowLabels : true,
+				//Interpolated JS string - can access value
+				scaleLabel : "<%=value%>",
+				//String - Scale label font declaration for the scale label
+				scaleFontFamily : "'Arial'",
+				//Number - Scale label font size in pixels	
+				scaleFontSize : 12,
+				//String - Scale label font weight style	
+				scaleFontStyle : "normal",
+				//String - Scale label font colour	
+				scaleFontColor : "#666",	
+				///Boolean - Whether grid lines are shown across the chart
+				scaleShowGridLines : true,
+				//String - Colour of the grid lines
+				scaleGridLineColor : "rgba(0,0,0,.05)",
+				//Number - Width of the grid lines
+				scaleGridLineWidth : 1,	
+				//Boolean - Whether the line is curved between points
+				bezierCurve : true,
+				//Boolean - Whether to show a dot for each point
+				pointDot : true,
+				//Number - Radius of each point dot in pixels
+				pointDotRadius : 3,
+				//Number - Pixel width of point dot stroke
+				pointDotStrokeWidth : 1,
+				//Boolean - Whether to show a stroke for datasets
+				datasetStroke : true,
+				//Number - Pixel width of dataset stroke
+				datasetStrokeWidth : 2,
+				//Boolean - Whether to fill the dataset with a colour
+				datasetFill : true,
+				//Boolean - Whether to animate the chart
+				animation : true,
+				//Number - Number of animation steps
+				animationSteps : 60,				
+				//String - Animation easing effect
+				animationEasing : "easeOutQuart",
+				//Function - Fires when the animation is complete
+				onAnimationComplete : null
+				
+			}
 			
 			var barChartOptions1 = {
 					//Boolean - If we show the scale above the chart data
@@ -76,9 +135,9 @@
 				labels : Months,
 				datasets : [
 					{
-						fillColor : "rgba(255,162,0,0.5)",
-						strokeColor : "rgba(255,162,0,1)",
-						pointColor : "rgba(255,162,0,1)",
+						fillColor : "rgba(250,105,0,0.5)",
+						strokeColor : "rgba(250,105,0,1)",
+						pointColor : "rgba(250,105,0,1)",
 						pointStrokeColor : "#fff",
 						data : <?php print_r($normalE);?>
 					},
@@ -118,26 +177,25 @@
 				labels : ["Current Emissions", "Emissions after installation"],
 				datasets : [
 					{
-						fillColor : "rgba(220,220,220,0.5)",
-						strokeColor : "rgba(220,220,220,1)",
-						pointColor : "rgba(220,220,220,1)",
+						fillColor : "rgba(250,105,0,0.5)",
+						strokeColor : "rgba(250,105,0,1)",
+						pointColor : "rgba(250,105,0,1)",
 						pointStrokeColor : "#fff",
-						data : [<?php print_r($Emission);?>, <?php if($emissionSolar > 0) { print_r($emissionSolar);} else {echo 0;} ?>]
-					}
-					// ,
-					// {
-					// 	fillColor : "rgba(151,187,205,0.5)",
-					// 	strokeColor : "rgba(151,187,205,1)",
-					// 	pointColor : "rgba(151,187,205,1)",
-					// 	pointStrokeColor : "#fff",
-					// 	data : [51, 35]
-					// }
+						data : [<?php print_r($Emission);?>, 0]
+					},
+					{
+						fillColor : "rgba(105,210,231,0.5)",
+						strokeColor : "rgba(105,210,231,1)",
+						pointColor : "rgba(105,210,231,1)",
+						pointStrokeColor : "#fff",
+						data : [0, <?php if($emissionSolar > 0) { print_r($emissionSolar);} else {echo 0;} ?>]
+					}					
 				]
 				
 			}
 			
 			var myLine1 = new Chart(document.getElementById("myChart1").getContext("2d")).Line(lineChartData1);
-			var myLine2 = new Chart(document.getElementById("myChart2").getContext("2d")).Line(lineChartData2);
+			var myLine2 = new Chart(document.getElementById("myChart2").getContext("2d")).Line(lineChartData2, lineChartOptions1);
 			var myLine3 = new Chart(document.getElementById("myChart3").getContext("2d")).Bar(barChartData1, barChartOptions1);
 	</script>
     </body>
